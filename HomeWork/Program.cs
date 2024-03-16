b95112eb0e007a3df1461a8ef072be7520a8b091
Kateryna Trushko <trushkokateryna@gmail.com>
2024-03-16 13:41:28 +0000

    Add project files.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using HomeWork;
+using System;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        User user1 = new User();
+        user1.Gender = User.UserGender.Female;
+
+        Console.WriteLine("Input username");
+        string userName = Console.ReadLine();
+        user1.FirstName = userName;
+
+        Console.WriteLine("Input lastname");
+        string lastName = Console.ReadLine();
+        user1.LastName = lastName;
+       
+        Console.WriteLine($"This is {user1.FirstName} {user1.LastName}, a gender is {user1.Gender.ToString().ToLower()} ");
+        Console.WriteLine(user1.ToString());
+
+    }
+}
---
+++ HomeWork/User.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+
+ 
+    public class User
+    {
+        public enum UserGender
+        {
+            Male,
+            Female
+        }
+
+        private string _firstName;
+        private string _lastName;
+        private string _age;
+        private UserGender _gender;
+
+        //Enum.IsDefined(typeof(UserGender), value)
+        public string FirstName 
+        {
+            get; set;
+/*          get => _firstName;
+            set => _lastName = value;*/
+        }
+
+        public string LastName
+        { get;  set; }
+
+        public string Age => _age;
+
+        public UserGender Gender
+        { get; set; }
+
+        
+        public User() { }
+
+        public User( string name, string lastName, string gender, string age )
+        {
+            
+            FirstName = name;
+            LastName = lastName;
+            _age = age;
+         
+                      
+              if ( gender.Trim().ToLower() == "male")
+               {
+                   Gender = UserGender.Male;
+               } else if ( gender.Trim().ToLower() == "female")
+               { 
+                   Gender = UserGender.Female; 
+               } else
+               {
+                 throw new ArgumentOutOfRangeException(nameof(gender), "Gender must be male or female");
+               }
+        }
+
+        public override string ToString()
+        {
+            return $"Hi, my name is {FirstName} and last name {LastName}. I am {20} years old. I am {Gender}.";
+        }
+
+    }
+}
